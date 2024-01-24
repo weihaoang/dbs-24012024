933c543b74959666428784dff6bc77378818ce0a
ANG WEI HAO <140620314+weihaoang@users.noreply.github.com>
2024-01-24 16:03:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
         rate = float(request.form.get("rate"))
         return(render_template("index.html",result=(rate*-50.6)+90.2))
     else:
-        return(render_template("index.html",result="Insert exchange rate here"))
+        return(render_template("index.html",result="________"))
 
 if __name__ == "__main__":
     app.run()
